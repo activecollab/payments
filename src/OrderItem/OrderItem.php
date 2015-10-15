e64117d6fce4607a61ddd089385c4814f93266b4
Ilija Studen <ilija.studen@activecollab.com>
2015-10-15 21:10:03 +0200

    Order item interface and implementation

--- src/OrderItem/OrderItem.php
+++ src/OrderItem/OrderItem.php
@@ -7,4 +7,54 @@ namespace ActiveCollab\Payments\OrderItem;
  */
 class OrderItem implements OrderItemInterface
 {
+    /**
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @var float|int
+     */
+    private $quantity = 1;
+
+    /**
+     * @var float
+     */
+    private $unit_cost = 0.0;
+
+    /**
+     * @param string        $description
+     * @param float|integer $quantity
+     * @param float         $unit_cost
+     */
+    public function __construct($description, $quantity, $unit_cost)
+    {
+        $this->description = trim($description);
+        $this->quantity = $quantity;
+        $this->unit_cost = $unit_cost;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return float|int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @return float
+     */
+    public function getUnitCost()
+    {
+        return $this->unit_cost;
+    }
 }
--- src/OrderItem/OrderItemInterface.php
+++ src/OrderItem/OrderItemInterface.php
@@ -7,4 +7,18 @@ namespace ActiveCollab\Payments\OrderItem;
  */
 interface OrderItemInterface
 {
+    /**
+     * @return string
+     */
+    public function getDescription();
+
+    /**
+     * @return float
+     */
+    public function getQuantity();
+
+    /**
+     * @return float
+     */
+    public function getUnitCost();
 }
