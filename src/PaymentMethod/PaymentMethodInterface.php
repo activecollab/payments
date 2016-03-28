d8a7af3bd06e5c137b6f41528f3e03b4ef5c860e
Ilija Studen <ilija.studen@activecollab.com>
2016-03-28 12:18:50 +0200

    Extend payment method interface and add credit card PM

--- src/PaymentMethod/PaymentMethodInterface.php
+++ src/PaymentMethod/PaymentMethodInterface.php
@@ -10,13 +10,26 @@ declare (strict_types = 1);
 
 namespace ActiveCollab\Payments\PaymentMethod;
 
+use ActiveCollab\DateValue\DateValueInterface;
+
 /**
  * @package ActiveCollab\Payments\PaymentMethod
  */
 interface PaymentMethodInterface
 {
+    const CREDIT_CARD = 'credit_card';
+    const PAY_PAL = 'pay_pal';
+    const APPLE_PAY = 'apple_pay';
+
     /**
-     * Return payment method's reference (ID).
+     * Return payment method type.
+     *
+     * @return string
+     */
+    public function getPaymentMethodType();
+
+    /**
+     * Return payment method's reference on the gateway (ID).
      *
      * @return string|null
      */
@@ -30,9 +43,37 @@ interface PaymentMethodInterface
     public function getOurReference();
 
     /**
+     * Return a reference that customer can use to indetify the payment method (masked card number, PayPal account etc).
+     * 
+     * @return string
+     */
+    public function getCustomerReference();
+
+    /**
+     * Return the date when this payment method is expected to expire.
+     *
+     * @return DateValueInterface
+     */
+    public function getExpiresOn();
+
+    /**
      * Return true if this payment method is active, or expired.
      *
      * @return bool
      */
     public function isExpired(): bool;
+
+    /**
+     * Return true if this payment method can be used more than once.
+     *
+     * @return bool
+     */
+    public function isReusable(): bool;
+
+    /**
+     * Return arguments that are needed so this payment method can be used again.
+     *
+     * @return mixed
+     */
+    public function getReuseArguments();
 }
