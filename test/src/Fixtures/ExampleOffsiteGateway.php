204d65e9e229c7f44b0a97a2453f0ec46a518714
Ilija Studen <ilijastuden@users.noreply.github.com>
2016-11-12 14:53:59 +0100

    Fix updateSubscription implementation

--- test/src/Fixtures/ExampleOffsiteGateway.php
+++ test/src/Fixtures/ExampleOffsiteGateway.php
@@ -129,19 +129,11 @@ class ExampleOffsiteGateway implements GatewayInterface
     }
 
     /**
-     * Update an existing interface for the given customer, that ordered a given product using a given payment method.
-     *
-     * @param SubscriptionInterface  $subscription
-     * @param CustomerInterface      $customer
-     * @param PaymentMethodInterface $payment_method
-     * @param                        $product_name
-     * @param string                 $period
-     * @param array                  ...$arguments
-     * @return SubscriptionInterface
+     * {@inheritdoc}
      */
     public function updateSubscription(SubscriptionInterface $subscription, CustomerInterface $customer, PaymentMethodInterface $payment_method, $product_name, string $period, ...$arguments):SubscriptionInterface
     {
-        // @TODO implement
+        return $subscription;
     }
 
     /**
