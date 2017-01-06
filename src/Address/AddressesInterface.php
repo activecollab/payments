a0126c7905308605f6a33ba3dbab00e584952c1b
Ilija Studen <ilija.studen@activecollab.com>
2017-01-07 00:11:35 +0100

    Add interfaces that are needed for address books

---
+++ src/Address/AddressesInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Payments project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Payments\Address;
+
+interface AddressesInterface
+{
+    public function getAddresses(): array;
+
+    public function addAddresses(AddressInterface ...$addresses): AddressesInterface;
+
+    public function clearAddresses(): AddressesInterface;
+}
---
+++ src/Address/RequiredDefaultAddressInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Active Collab Payments project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\Payments\Address;
+
+interface RequiredDefaultAddressInterface
+{
+    public function getDefaultAddress(): AddressInterface;
+
+    public function setDefaultAddressInterface(AddressInterface $address): AddressesInterface;
+}
